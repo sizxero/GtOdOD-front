e849e70fc941e5aabb15d2e3a4c7241651fe8aae
박유경 <pyk0514@kumoh.ac.kr>
2022-09-21 17:27:31 +0900

    [chore] 로그인 여부에 따라 홈화면 다르게 수정

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { Login, SignUp, ToDoList } from "./pages";
 import { Provider } from 'react-redux';
 import store from './redux/Store';
 import rootReducer from './redux/RootReducer';
+import { getCookie } from './client/Cookie';
+
 const App = () => {
 	return (
 		<Provider store={store(rootReducer)}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Logo />
             <Routes>
-                <Route path='/' element={<ToDoList />} />
+                {getCookie("token") === null || getCookie("token") === undefined || getCookie("token") === ''
+                ? <Route path='/' element={<Login />} />
+                : <Route path='/' element={<ToDoList />} />
+                }
                 <Route path='/login' element={<Login />} />
                 <Route path='/signup' element={<SignUp />} />
             </Routes>
